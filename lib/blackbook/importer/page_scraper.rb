e593d6a7ac3665ca0b63cad03c7f546e90ccee82
Joshua Slayton <joshuaxls@gmail.com>
2010-12-13 19:42:31 -0800

    make compatible with ruby 1.9?

--- lib/blackbook/importer/page_scraper.rb
+++ lib/blackbook/importer/page_scraper.rb
@@ -1,38 +1,39 @@
 require 'rubygems'
 gem 'mechanize', '>= 0.7.0'
 require 'mechanize'
-require 'generator' # for SyncEnumerator
 
 # Patch Mechanize's broken html unescaping Mechanize 0.6.11
 class WWW::Mechanize
-  def to_absolute_uri(url, cur_page=current_page())
-    unless url.is_a? URI
-      url = url.to_s.strip
-      url = URI.parse(
-              Util.html_unescape(
-                SyncEnumerator.new(
-                  url.split(/%[0-9A-Fa-f]{2}/), url.scan(/%[0-9A-Fa-f]{2}/)
-                ).map { |x,y|
-                  "#{URI.escape(x||'')}#{y}"
-                }.join('').gsub(/%23/, '#')
+  if false
+    def to_absolute_uri(url, cur_page=current_page())
+      unless url.is_a? URI
+        url = url.to_s.strip
+        url = URI.parse(
+                Util.html_unescape(
+                  SyncEnumerator.new(
+                    url.split(/%[0-9A-Fa-f]{2}/), url.scan(/%[0-9A-Fa-f]{2}/)
+                  ).map { |x,y|
+                    "#{URI.escape(x||'')}#{y}"
+                  }.join('').gsub(/%23/, '#')
+                )
               )
-            )
-      # Mechanize here uses #zip to combine the two arrays, which will ignore 
-      # excessive elements of the second array (the one which is passed as an 
-      # argument). That means if the URL ends with more than one already escaped
-      # character, then only the first one will be restored into the resulting
-      # URL.
-    end
+        # Mechanize here uses #zip to combine the two arrays, which will ignore 
+        # excessive elements of the second array (the one which is passed as an 
+        # argument). That means if the URL ends with more than one already escaped
+        # character, then only the first one will be restored into the resulting
+        # URL.
+      end
 
-    # construct an absolute uri
-    if url.relative?
-      raise 'no history. please specify an absolute URL' unless cur_page.uri
-      url = cur_page.uri + url
-      # Strip initial "/.." bits from the path
-      url.path.sub!(/^(\/\.\.)+(?=\/)/, '')
-    end
+      # construct an absolute uri
+      if url.relative?
+        raise 'no history. please specify an absolute URL' unless cur_page.uri
+        url = cur_page.uri + url
+        # Strip initial "/.." bits from the path
+        url.path.sub!(/^(\/\.\.)+(?=\/)/, '')
+      end
 
-    return url
+      return url
+    end
   end
 end
 
